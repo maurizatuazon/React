134b6daeccf7d06b1ba73e379d97802f4619b259
Mau Tuazon <MauT@Magenic.com>
2018-11-12 22:32:43 +0800

    Added show/hide of recipes and added initial implementation for recipe form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,97 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class RecipeForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        recipeTitle : '',
+        recipeDescription : ''
+    };
+ 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A recipe title was submitted: ' + this.state.recipeTitle);
+    event.preventDefault();
+  }
+
+  render() {
+    return(
+      <div className="RecipeForm">
+        <form onSubmit={this.handleSubmit}>
+          <label>Recipe Title: 
+            <input 
+              name="recipeTitle"
+              type="text"
+              value={this.state.recipeTitle}
+              onChange={this.handleChange}
+            />  
+          </label>
+          <br />
+          <label>Recipe Description: 
+            <textarea 
+              name="recipeDescription"
+              type="text"
+              value={this.state.recipeDescription}
+              onChange={this.handleChange}
+            />  
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>  
+    );
+  }
+}
+
+class RecipeList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isRecipesShown: this.props.isRecipesShown,
+      recipes : this.props.recipes,
+      showRecipeDetail : false
+    };
+
+    this.handleToggleShowRecipeDetail = this.handleToggleShowRecipeDetail.bind(this);
+  }
+
+  handleToggleShowRecipeDetail() {
+    this.setState(state => ({
+      showRecipeDetail: !state.showRecipeDetail
+    }));
+  }
+
+  render() {
+    if (!this.props.isRecipesShown) {
+      return null;
+    }
+
+    const listItems = this.state.recipes.map((recipeItem, index) =>
+      <RecipeDetail key={recipeItem.title.toString()}
+                recipe={recipeItem} />
+    );
+
+    return(
+      <div>
+        {listItems}
+      </div>
+    )
+  }
+}
+
 class RecipeDetail extends Component {
   render() {
     return(
-      <div className="Recipe">
+      <div className="RecipeDetail">
         <h2>{this.props.recipe.title}</h2>
         <span>{this.props.recipe.description}</span>
         <br/><br/>
@@ -16,6 +103,7 @@ class RecipeDetail extends Component {
         <span>{this.props.recipe.procedure}</span>
         <br/><br/>
         <h4>Cooking Time: {this.props.recipe.cookingTime} | Prep Time: {this.props.recipe.prepTime}</h4>
+        <hr/>
       </div>
     )
   }
@@ -37,16 +125,17 @@ class App extends Component {
           cookingTime: "30 mins",
           isFavorite: true
       },
+      recipes : [],
       showRecipes: false,
     };
 
-    this.onShowRecipes = this.onShowRecipes.bind(this);
+    this.handleToggleShowRecipes = this.handleToggleShowRecipes.bind(this);
   }
 
-  onShowRecipes() {
-    this.setState({
-      showRecipes: true,
-    });
+  handleToggleShowRecipes() {
+    this.setState(state => ({
+      showRecipes: !state.showRecipes
+    }));
   }
 
   render() {
@@ -62,12 +151,16 @@ class App extends Component {
           >
             Learn React
           </a>
-          <br/>
-          
           <h1>Recipes</h1>
-          <button onClick={this.onShowRecipes}>Show Recipes</button>
         </header>
-        {this.state.showRecipes ? <RecipeDetail recipe={this.state.recipe} /> : null}
+        <br/>
+        <div>
+          <button onClick={this.handleToggleShowRecipes}>
+            {this.state.showRecipes ? 'Hide' : 'Show'} Recipes
+          </button>
+          <RecipeForm />
+          <RecipeList isRecipesShown={this.state.showRecipes} recipes={[this.state.recipe, this.state.recipe]}/> 
+        </div>
       </div>
     );
   }
