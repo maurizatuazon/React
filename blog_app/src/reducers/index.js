0f80963d432e339d7e9fef7241211d23a2377d87
Mau Tuazon <MauT@Magenic.com>
2019-01-14 21:07:34 +0800

    react redux apps

--- blog_app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import postReducer from './postReducer';
-import usersReducer from './usersReducer';
-
-export default combineReducers({
-    posts: postReducer,
-    users: usersReducer
-});
