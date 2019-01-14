78e74fbba4a8777aaa629f659f1fd08b1fb14570
Mau Tuazon <MauT@Magenic.com>
2019-01-14 23:18:26 +0800

    remove key

--- videos_app/src/api/youtube.js
+++ videos_app/src/api/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyAVlSgILu0zgqYhqKH7Ex39aj_LEgk9YqU';
+const KEY = 'change me';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
